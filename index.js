cdad04d6c3fe7b47a157740f8c089427095abd08
fazekas <fazekasxluca98@gmail.com>
2024-03-31 15:21:15 +0200

    Revert "modositott repo"
    This reverts commit 9689ce6cf8e190fffd5261131a01c0379e03c60b.

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function parosKivalogat(szamok) {
     }
     return parosSZ;
 }
-//asdasd
 var szamokTomb = [3, 2, 5, 7, 8];
 console.log(parosKivalogat(szamokTomb));
 var d1 = { nev: "kiss lajos", kor: 23, aktiv: false };
